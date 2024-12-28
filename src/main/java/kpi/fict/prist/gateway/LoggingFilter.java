8667796984842b6a30d14e1d13ec4c32a5c32ed7
Bogdan Zaranik <bogdan.zaranik@appliedmethods.biz>
2024-12-28 20:08:04 +0200

    setup gateway for admintool

---
+++ src/main/java/kpi/fict/prist/gateway/LoggingFilter.java
@@ -0,0 +1,32 @@
+package kpi.fict.prist.gateway;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.*;
+
+@Slf4j
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        Set<URI> uris = exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
+        String originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
+        Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+        URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+        assert route != null;
+        log.info("Incoming request {} is routed to id: {}, uri: {} with order {}", originalUri, route.getId(), routeUri, route.getOrder());
+        return chain.filter(exchange);
+    }
+}
